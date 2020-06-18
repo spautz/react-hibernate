2e29914c98c235c9b90c325b3e0dac55350b6ffe
Steven Pautz <spautz@gmail.com>
2020-06-17 20:24:22 -0400

    Revert to earlier prepare script to (hopefully) fix CI

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,5 @@
 /* eslint-env node */
 
-module.exports = (api) => {
-  // https://stackoverflow.com/questions/53327625/cannot-find-module-babel-plugin-transform-react-jsx-source-when-running-react
-  api.cache(true);
-  return {
-    presets: ['@babel/react', '@babel/preset-typescript'],
-  };
+module.exports = {
+  presets: ['@babel/react', '@babel/preset-typescript'],
 };
