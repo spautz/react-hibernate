9e278f9cb944228760dee104776d811626333465
Steven Pautz <spautz@gmail.com>
2020-10-01 19:33:36 -0400

    Lock down devDependencies

--- packages/dev-helpers/src/jest/renderWithRouterContext.tsx
+++ packages/dev-helpers/src/jest/renderWithRouterContext.tsx
@@ -1,17 +1,17 @@
 import React, { ReactElement, ComponentType } from 'react';
-import { createMemoryHistory, MemoryHistoryBuildOptions } from 'history';
+import { createMemoryHistory, MemoryHistoryOptions } from 'history';
 import { isElement } from 'react-is';
-import { Router } from 'react-router';
+import { MemoryRouter } from 'react-router';
 import { render } from '@testing-library/react';
 
 const renderWithRouterContext = (
   AppRoot: ReactElement | ComponentType,
-  historyOptions?: MemoryHistoryBuildOptions,
+  historyOptions?: MemoryHistoryOptions,
 ): ReturnType<typeof render> => {
   const history = createMemoryHistory(historyOptions);
 
   const renderResult = render(
-    <Router history={history}>{isElement(AppRoot) ? AppRoot : <AppRoot />}</Router>,
+    <MemoryRouter>{isElement(AppRoot) ? AppRoot : <AppRoot />}</MemoryRouter>,
   );
   return {
     ...renderResult,
