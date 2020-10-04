649f92e39f9018875855715c9325a487e6bfe748
Steven Pautz <spautz@gmail.com>
2020-10-04 18:37:13 -0400

    Test coverage

---
+++ packages/react-pauseable-containers/src/PauseableContextContainer.test.tsx
@@ -0,0 +1,67 @@
+/* eslint-env jest */
+import '@testing-library/jest-dom/extend-expect';
+import * as React from 'react';
+import { render, screen } from '@testing-library/react';
+
+import { PauseableContextContainer } from '.';
+import { Context, useContext } from 'react';
+
+describe('PauseableContextContainer', () => {
+  let TestContext: Context<string>;
+  let TestConsumer: React.FC;
+  beforeEach(() => {
+    TestContext = React.createContext('default');
+    TestConsumer = () => {
+      const valueFromContext = useContext(TestContext);
+      return <span>{valueFromContext}</span>;
+    };
+  });
+
+  it('allows updates', () => {
+    const { rerender } = render(
+      <TestContext.Provider value="one">
+        <PauseableContextContainer Context={TestContext} shouldUpdate={true}>
+          <TestConsumer />
+        </PauseableContextContainer>
+      </TestContext.Provider>,
+    );
+
+    expect(screen.queryByText('one')).toBeInTheDocument();
+
+    // "one" -> "two"
+    rerender(
+      <TestContext.Provider value="two">
+        <PauseableContextContainer Context={TestContext} shouldUpdate={true}>
+          <TestConsumer />
+        </PauseableContextContainer>
+      </TestContext.Provider>,
+    );
+
+    expect(screen.queryByText('one')).not.toBeInTheDocument();
+    expect(screen.queryByText('two')).toBeInTheDocument();
+  });
+
+  it('prevents updates', () => {
+    const { rerender } = render(
+      <TestContext.Provider value="one">
+        <PauseableContextContainer Context={TestContext} shouldUpdate={false}>
+          <TestConsumer />
+        </PauseableContextContainer>
+      </TestContext.Provider>,
+    );
+
+    expect(screen.queryByText('one')).toBeInTheDocument();
+
+    // "one" -> "two"
+    rerender(
+      <TestContext.Provider value="two">
+        <PauseableContextContainer Context={TestContext} shouldUpdate={false}>
+          <TestConsumer />
+        </PauseableContextContainer>
+      </TestContext.Provider>,
+    );
+
+    expect(screen.queryByText('one')).toBeInTheDocument();
+    expect(screen.queryByText('two')).not.toBeInTheDocument();
+  });
+});
