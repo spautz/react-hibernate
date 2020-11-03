46f4e88df5d8e255b785576b9c9fbfb55612e627
Steven Pautz <spautz@gmail.com>
2020-11-03 12:14:54 -0500

    Dev environment updates

--- scripts/build-everything.sh
+++ scripts/build-everything.sh
@@ -15,8 +15,10 @@ run_command "./scripts/check-environment.sh"
 run_command "yarn install"
 
 ###################################################################################################
-# Run all read-write scripts and read-only scripts. This is overkill and duplicates some work, but
-# also helps catch intermittent errors. Suitable for running before lunch or teatime.
+# Run all read-write scripts and read-only scripts. This is overkill and duplicates a lot of work,
+# but also helps catch intermittent errors. Suitable for running before lunch or teatime.
 
 run_command "yarn all"
 run_command "yarn all:readonly"
+run_command "yarn packages:all"
+run_command "yarn packages:all:readonly"
