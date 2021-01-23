2fc11ad1ec8294e4e7677d30480c1d14076db0f3
Steven Pautz <spautz@gmail.com>
2021-01-23 14:15:06 -0500

    Fix typo

--- scripts/branch-coverage-reporting.sh
+++ scripts/branch-coverage-reporting.sh
@@ -23,15 +23,15 @@ echo "ORIGINAL_COVERALLS_SERVICE_JOB_ID=${ORIGINAL_COVERALLS_SERVICE_JOB_ID}"
 # If we're on the main branch, report code coverage separately for each project
 if [ "${CURRENT_BRANCH}" = 'master' ] || true; then
   for DIR in ./packages/*/; do
-    $COVERAGE_BRANCH = "x-cov-$(echo $DIR | sed -e 's/[^-a-z]//gi')"
+    $COVERAGE_REPORTING_BRANCH="x-cov-$(echo $DIR | sed -e 's/[^-a-z]//gi')"
 
-    echo "setting GITHUB_REF=refs/heads/${COVERAGE_BRANCH}"
-    export GITHUB_REF="refs/heads/${COVERAGE_BRANCH}"
-    echo "setting GITHUB_HEAD_REF=refs/heads/${COVERAGE_BRANCH}"
-    export GITHUB_HEAD_REF="refs/heads/${COVERAGE_BRANCH}"
+    echo "setting GITHUB_REF=refs/heads/${COVERAGE_REPORTING_BRANCH}"
+    export GITHUB_REF="refs/heads/${COVERAGE_REPORTING_BRANCH}"
+    echo "setting GITHUB_HEAD_REF=refs/heads/${COVERAGE_REPORTING_BRANCH}"
+    export GITHUB_HEAD_REF="refs/heads/${COVERAGE_REPORTING_BRANCH}"
 
-#    export COVERALLS_SERVICE_JOB_ID="${ORIGINAL_COVERALLS_SERVICE_JOB_ID}:${COVERAGE_BRANCH}"
-    export COVERALLS_GIT_BRANCH=$COVERAGE_BRANCH
+#    export COVERALLS_SERVICE_JOB_ID="${ORIGINAL_COVERALLS_SERVICE_JOB_ID}:${COVERAGE_REPORTING_BRANCH}"
+    export COVERALLS_GIT_BRANCH=$COVERAGE_REPORTING_BRANCH
     run_command "yarn --cwd=${DIR} test:report-local"
   done
 fi
