cf205cc0f89df5c5e2bbdd1644daa9d7453568dd
Steven Pautz <spautz@gmail.com>
2021-01-23 16:27:06 -0500

    Try non-numeric TRAVIS_BUILD_NUMBER

--- scripts/branch-coverage-reporting.sh
+++ scripts/branch-coverage-reporting.sh
@@ -22,20 +22,21 @@ if [ "${CURRENT_BRANCH}" = 'master' ] || true; then
   # Allow Coveralls to receive multiple reports from a single job
   COVERALLS_PARALLEL=true
   ORIGINAL_TRAVIS_JOB_ID=$TRAVIS_JOB_ID
+  ORIGINAL_TRAVIS_BUILD_NUMBER=$TRAVIS_BUILD_NUMBER
+
+  echo "TRAVIS_BUILD_NUMBER=${TRAVIS_BUILD_NUMBER}"
 
   for DIR in ./packages/*/; do
     DIR_IDENTIFIER=$(echo $DIR | sed -e 's/packages//gi' | sed -e 's/[^-a-z]//gi')
     COVERAGE_REPORTING_BRANCH="x-cov-${DIR_IDENTIFIER}"
 
-#    echo "setting GITHUB_REF=refs/heads/${COVERAGE_REPORTING_BRANCH}"
-#    export GITHUB_REF="refs/heads/${COVERAGE_REPORTING_BRANCH}"
-#    echo "setting GITHUB_HEAD_REF=refs/heads/${COVERAGE_REPORTING_BRANCH}"
-#    export GITHUB_HEAD_REF="refs/heads/${COVERAGE_REPORTING_BRANCH}"
-#
-#    export COVERALLS_SERVICE_JOB_ID="$(git rev-parse --short HEAD)-${COVERAGE_REPORTING_BRANCH}"
-#    export COVERALLS_GIT_BRANCH=$COVERAGE_REPORTING_BRANCH
+    # With node-coveralls package and Travis, we have to set these env variables to control what gets sent to
+    # Coveralls. Other services use other env vars:
+    # https://github.com/nickmerwin/node-coveralls/blob/master/lib/getOptions.js#L25-L32
     TRAVIS_BRANCH=$COVERAGE_REPORTING_BRANCH
+    TRAVIS_BUILD_NUMBER="${ORIGINAL_TRAVIS_BUILD_NUMBER}-${COVERAGE_REPORTING_BRANCH}"
     TRAVIS_JOB_ID="${ORIGINAL_TRAVIS_JOB_ID}-${COVERAGE_REPORTING_BRANCH}"
+
     run_command "yarn --cwd=${DIR} test:report-local"
   done
 fi
