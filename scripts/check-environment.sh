2b4211f60d5e45c122d5a8cb2c55ebf6988d5ac1
Steven Pautz <spautz@gmail.com>
2021-01-23 12:57:51 -0500

    Experiment with per-branch code coverage reporting

--- scripts/check-environment.sh
+++ scripts/check-environment.sh
@@ -11,13 +11,13 @@ source scripts/helpers.sh
 ###################################################################################################
 # Check versions of Node, Yarn, and any other tools required
 
-# If "engines" is set in package.json, validate against its values
+# If package.json's "engines" is set, validate against its values
 HAS_ENGINES=$(get_json_value package.json "engines")
 if [ $HAS_ENGINES ]; then
   run_npm_command check-node-version --package --print
 fi
 
-# If a "node" value is not set in package.json's "engines", validate against .nvmrc
+# If package.json's "engines" does not specify a node version, validate against .nvmrc instead
 HAS_ENGINES_NODE=$(get_json_value package.json "node")
 if [ ! $HAS_ENGINES ] || [ ! $HAS_ENGINES_NODE ]; then
   NODE_ACTUAL=$(echo $(node --version) | tr -d '[v:space:]')
--- scripts/full-ci.sh
+++ scripts/full-ci.sh
@@ -11,10 +11,23 @@ source scripts/helpers.sh
 ###################################################################################################
 # Setup: Node should already have been set up in the environment init
 
-run_command "./scripts/check-environment.sh"
-run_command "yarn install --frozen-lockfile"
+#run_command "./scripts/check-environment.sh"
+#run_command "yarn install --frozen-lockfile"
+#
+####################################################################################################
+#
+## First run the "real" CI checks
+#run_command "yarn ci"
 
-###################################################################################################
-# Run whatever the script in package.json wants to run
+# If we're on the main branch, run code coverage per-branch
+CURRENT_BRANCH=$(git branch --show-current)
 
-run_command "yarn ci"
+if [ "${CURRENT_BRANCH}" = 'master' ] || true; then
+  for dir in ./packages/*/; do
+    echo "setting GITHUB_REF=refs/heads/x-cov-${dir}"
+    export GITHUB_REF="refs/heads/x-cov-${dir}"
+    echo "setting GITHUB_HEAD_REF=refs/heads/x-cov-${dir}"
+    export GITHUB_HEAD_REF="refs/heads/x-cov-${dir}"
+    run_command "yarn --cwd=${dir} test:report-local"
+  done
+fi
